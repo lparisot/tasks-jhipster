d97e563cf2eac8299e7ac6e5252401e586bffa11
L.P <parisot.laurent@gmail.com>
2017-11-26 12:21:18 +0100

    Add Task entity

--- src/main/java/com/lpa/tasks/config/CacheConfiguration.java
+++ src/main/java/com/lpa/tasks/config/CacheConfiguration.java
@@ -41,6 +41,7 @@ public class CacheConfiguration {
             cm.createCache(com.lpa.tasks.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.lpa.tasks.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.lpa.tasks.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.lpa.tasks.domain.Task.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
